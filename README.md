2e3afc3cb50c714e2db1d9304a2a7c2109b07fb5
Mattia Cimadomo <cimadomomattia@gmail.com>
2023-07-03 22:19:34 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,128 @@
-# Google-Drive-Backup
+﻿<p align="center">
+    <h1 align="center">Google Drive Backup Script</h1>
+    <p align="center">An automatic, storage-space friendly and easy way to backup your Google Drive Data</p>
+</p>
+
+---
+
+## 📖 Description
+
+Unable to find a program that backs up my Google Drive folder on the internet, I decided to create one myself. This script is designed to automatically download and backup the entirety of a Google Drive folder. It neatly packages all files and directories into a zip file and, in the event of needing a new authentication token or the starting and conclusion of the backup process, will send notifications via email.
+
+
+## 🌟 Features
+
+- 📥 Downloads all files and directories from Google Drive.
+- 🗜️ Zips the downloaded data for efficient storage.
+- 📧 Emails notifications at the start and end of the backup process, as well as when a new token is needed for Google Drive API.
+- 🧹 Automatically deletes the downloaded data after a successful backup.
+
+
+## 🔍 Prerequisites
+
+Before running the script, ensure that you have the following prerequisites:
+
+- Python 3.6 or higher installed on your machine.
+- Python added to your system's PATH environment variable.
+
+
+## ⚙️ Setup and Installation
+
+1. **Clone the repository**
+    You can clone the repository using the following command:
+
+    ```shell
+    git clone https://github.com/tia-cima/Google-Drive-Backup.git
+    cd google-drive-backup
+    ```
+
+2. **Install dependencies**
+    Use pip to install the necessary Python libraries:
+
+    ```shell
+    pip install -r requirements.txt
+    ```
+
+3. **Set up Google Drive API**
+    This script uses Google Drive API to access and download your data. Follow the steps below to set up the API:
+
+    1. **Create a new project on Google Cloud Platform (GCP)**:
+       - Go to the [Google Cloud Console](https://console.cloud.google.com/).
+       - Click the project drop-down and select or create the project for which you want to add an API key.
+       - Click the hamburger menu in the top left and select **APIs & Services** > **Library**.
+
+    2. **Enable Google Drive API for your project**:
+       - In the API Library, search for "Google Drive API".
+       - Click on "Google Drive API" in the search results.
+       - Click on **Enable**.
+
+    3. **Create credentials for the Google Drive API**:
+       - Go to **APIs & Services** > **Credentials**.
+       - Click on **+ CREATE CREDENTIALS** and select **OAuth client ID**.
+       - Configure the OAuth consent screen as needed. For the User Type, you can select "External" and click **Create**.
+       - Fill in the necessary information for the OAuth consent screen and click **Save and Continue**.
+       - For the Application type, select **Desktop app** and give it a name.
+       - Click on **Create**.
+       - Your client ID and client secret will be displayed. These values will be used in your Python script to access the Google Drive API.
+
+    4. **Download the JSON file with your credentials**:
+       - After creating your credentials, go back to the **Credentials** page.
+       - In the **OAuth 2.0 Client IDs** section, find the client you just created.
+       - Click on the download icon to download the JSON file. This file will be referred to as your "client_json" in the script.
+       - Keep this JSON file secure, as it contains sensitive information about your application's access permissions.
+
+    Note: The first time you run the script, you will be prompted to authorize the app with Google Drive. After authorizing, a new file `token.pickle` will be created. This token will be used for future authentications and you will not be prompted to authorize again unless the token becomes invalid.
+
+4. **Set up SendGrid**
+    This script uses SendGrid for email notifications. You'll need to create an API key in your SendGrid account. Here are the steps:
+
+    1. **Create a new SendGrid account**:
+       - Visit the [SendGrid website](https://sendgrid.com/) and sign up for a free account.
+
+    2. **Verify your email address**:
+       - After signing up, verify your email address by clicking on the link in the email sent by SendGrid.
+
+    3. **Create a new API Key**:
+       - Once you're in the SendGrid dashboard, go to **Email API** on the left navigation bar and click on **Integration Guide**.
+       - Select **Web API** (not SMTP Relay).
+       - Click on **Choose** under **Integrate using our Web API or SMTP relay**.
+       - Give your API Key a name and select either **Full Access** or **Restricted Access**. For this application, we only need the ability to send mail.
+       - In the **Restricted Access** section, expand **Mail Send** and switch the toggle to **Enabled**.
+       - Click on **Create & View** to create the new API Key.
+       - Your new API Key will be displayed. Copy and save this key securely. Remember that this is the only time you'll be able to access this key via the SendGrid interface. If you lose it, you'll have to create a new one.
+
+    4. **Enable "Mail Send" permissions**:
+       - Go to the **API Keys** section under **Settings**.
+       - Click on the name of the API Key you've just created.
+       - In the **API Key Permissions** section, set **Mail Send** to **Full Access**.
+
+    Remember to replace the `SENDGRID_API_KEY` variable in the script with your generated API key.
+    Your SendGrid API setup is now complete! Keep your API key secure as it allows sending emails from your account. In the backup script, you will use this key as the `sendgrid_api_key` variable. You can now send transactional emails through your Python application.
+    Note: With a free SendGrid account, you can send up to 100 emails per day. If you need to send more than that, you will need to upgrade your account.
+
+5. **Run the script**
+    You can run the script using the following command: 
+
+    `python backup.py --download_folder <folder to download files> --backup_folder <folder to save the backup> --sendgrid_api_key <your SendGrid API key> --from_email <email to send notifications from> --to_email <email to send notifications to> --client_json <path to your Google client json> --hour <hour to start the backup> --minute <minute to start the backup>`
+
+    - `download_folder`: This is the folder where the files from Google Drive will be downloaded.
+    - `backup_folder`: This is the folder where the downloaded files will be zipped and saved as a backup.
+    - `sendgrid_api_key`: Your SendGrid API key for sending email notifications.
+    - `from_email`: The email address that will be used to send notifications.
+    - `to_email`: The email address that will receive notifications.
+    - `client_json`: The path to your Google client json file. This is used to authenticate the script with Google Drive.
+    - `hour` and `minute`: The time at which the script will run every day.
+    
+    Replace the arguments with your desired settings. The script will run continuously and start the backup process at the given hour and minute.
+
+
+## 💡 Recommended Settings
+
+The 'token.pickle' file, which is used for Google Drive authentication, will be saved in the directory above the download and backup folders. For instance, if your download folder is "C:\Users\your_user\Desktop\test\d" and your backup folder is "C:\Users\your_user\Desktop\test\b", the 'token.pickle' file will be saved in "C:\Users\your_user\Desktop\test". Please ensure that you have write access to this directory.
+
+I also recommend to put, in the future 'token.pickle' folder, the 'client_json...' file, in order to have all in one single folder.
+
+
+## 📄 License
+
+This project is licensed under the terms of the MIT license. See the [LICENSE](https://github.com/tia-cima/Google-Drive-Backup/blob/master/LICENSE.txt) file for details.
---
+++ backup.py
@@ -0,0 +1,176 @@
+import argparse
+import datetime
+import io
+import os
+import pickle
+import re
+import time
+import zipfile
+from shutil import rmtree
+
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+
+def sanitize_filename(filename):
+    filename = re.sub(r'[^\x00-\x7f]', r'', filename)  # remove non-ASCII characters
+    filename = re.sub(r'[\\/:"*?<>|]+', r'', filename)  # remove illegal characters
+    return filename.rstrip()  # remove trailing whitespace
+
+def calculate_size(file_path):
+    if os.path.isfile(file_path):
+        return os.path.getsize(file_path) / (1024 ** 3)  # size in GB
+    else:
+        total_size = 0
+        for path, dirs, files in os.walk(file_path):
+            for f in files:
+                fp = os.path.join(path, f)
+                total_size += os.path.getsize(fp)
+        return total_size / (1024 ** 3)  # size in GB
+
+def send_email(subject, content, from_email, to_email, sendgrid_api_key):
+    message = Mail(
+        from_email=from_email,
+        to_emails=to_email,
+        subject=subject,
+        html_content=content
+    )
+    try:
+        sg = SendGridAPIClient(sendgrid_api_key)
+        response = sg.send(message)
+    except Exception as e:
+        print(str(e))
+
+def check_creds(client_json, from_email, to_email, sendgrid_api_key, token_path):
+    creds = None
+    if os.path.exists(token_path):
+        with open(token_path, 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            send_email("[Notification] New Token Needed", 
+                       "<div style='background-color: yellow;'>Your Google Drive backup program requires a new authentication token. Please run the program manually to obtain a new token.</div>", 
+                       from_email, to_email, sendgrid_api_key)
+            flow = InstalledAppFlow.from_client_secrets_file(client_json, SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open(token_path, 'wb') as token:
+            pickle.dump(creds, token)
+    return creds
+
+def download_file(service, item, filepath):
+    print(f"Downloading file {filepath}.")
+    request = service.files().get_media(fileId=item['id'])
+    fh = io.FileIO(filepath, 'wb')
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print(f"Download {int(status.progress() * 100)}%.")
+
+def download_drive_data(service, download_folder, parent='root', path=''):
+    skipped_files = []
+    page_token = None
+    while True:
+        results = service.files().list(q=f"'{parent}' in parents",
+                                       fields="nextPageToken, files(id, name, mimeType)",
+                                       pageToken=page_token).execute()
+        items = results.get('files', [])
+        for item in items:
+            filename = sanitize_filename(re.sub(r'[^\x00-\x7f]', r'', item['name']))
+            folderpath = os.path.join(download_folder, path)
+            os.makedirs(folderpath, exist_ok=True)
+            filepath = os.path.join(folderpath, filename)
+            if item['mimeType'] == 'application/vnd.google-apps.folder':
+                download_drive_data(service, download_folder, item['id'], os.path.join(path, filename))
+            else:
+                try:
+                    download_file(service, item, filepath)
+                except Exception as e:
+                    print(f"Error downloading file {filepath}. Error: {str(e)}. Skipping this file.")
+                    skipped_files.append(filepath)
+        page_token = results.get('nextPageToken', None)
+        if page_token is None:
+            break
+    return skipped_files
+
+def zip_data(backup_folder, download_folder):
+    print("Zipping process started.")
+    zipf = zipfile.ZipFile(backup_folder + '.zip', 'w', zipfile.ZIP_DEFLATED)
+    for root, dirs, files in os.walk(download_folder):
+        for file in files:
+            zipf.write(os.path.join(root, file), arcname=file)
+    zipf.close()
+    print("Zipping process ends.")
+    return backup_folder + '.zip'
+
+def backup_drive(backup_folder, download_folder, from_email, to_email, sendgrid_api_key, client_json):
+    token_path = os.path.join(os.path.dirname(download_folder), 'token.pickle')
+    creds = check_creds(client_json, from_email, to_email, sendgrid_api_key, token_path)
+
+    service = build('drive', 'v3', credentials=creds)
+    try:
+        send_email("[Starting] Google Drive backup started", 
+                   "<div style='background-color: yellow;'>Your backup process has started.</div>", 
+                   from_email, to_email, sendgrid_api_key)
+        
+        skipped_files = download_drive_data(service, download_folder)
+        
+        zip_file_path = zip_data(backup_folder, download_folder)
+        size_after = calculate_size(zip_file_path)
+        if len(skipped_files) > 0:
+            send_email("[Success] Google Drive backup succeeded", 
+                       f"<div style='background-color: green;'>Your backup was successful. The final backup size is {size_after:.2f} GB.</div><br>Following files were skipped:<br> {'<br>'.join(skipped_files)}", 
+                       from_email, to_email, sendgrid_api_key)
+        else:
+            send_email("[Success] Google Drive backup succeeded", 
+                       f"<div style='background-color: green;'>Your backup was successful. The final backup size is {size_after:.2f} GB.</div>", 
+                       from_email, to_email, sendgrid_api_key)
+    except Exception as e:
+        send_email("[Failed] Google Drive backup failed", 
+                   f"<div style='background-color: red;'>Your backup failed. Error: {str(e)}</div>", 
+                   from_email, to_email, sendgrid_api_key)
+    finally:
+        for filename in os.listdir(download_folder):
+            file_path = os.path.join(download_folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    rmtree(file_path)
+            except Exception as e:
+                print(f'Failed to delete {file_path}. Reason: {e}')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--download_folder', required=True, help='Folder where files will be downloaded')
+    parser.add_argument('--backup_folder', required=True, help='Folder where backup will be saved')
+    parser.add_argument('--sendgrid_api_key', required=True, help='Your SendGrid API key')
+    parser.add_argument('--from_email', required=True, help='Email to send notifications from')
+    parser.add_argument('--to_email', required=True, help='Email to send notifications to')
+    parser.add_argument('--client_json', required=True, help='Path to your Google client json')
+    parser.add_argument('--hour', type=int, required=True, help='Hour to start the backup')
+    parser.add_argument('--minute', type=int, required=True, help='Minute to start the backup')
+    
+    args = parser.parse_args()
+
+    DOWNLOAD_FOLDER = args.download_folder
+    BACKUP_FOLDER = args.backup_folder
+    SENDGRID_API_KEY = args.sendgrid_api_key
+    FROM_EMAIL = args.from_email
+    TO_EMAIL = args.to_email
+    CLIENT_JSON = args.client_json
+    HOUR = args.hour
+    MINUTE = args.minute
+
+    while True:
+        current_time = datetime.datetime.now()
+        if (current_time.hour == HOUR and current_time.minute == MINUTE):
+            backup_drive(BACKUP_FOLDER, DOWNLOAD_FOLDER, FROM_EMAIL, TO_EMAIL, SENDGRID_API_KEY, CLIENT_JSON)
+        time.sleep(60)
